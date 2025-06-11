c285f6f1d34c8d9b0631dfa9d0a29520f8e80270
balabaladanny <danny910208@gmail.com>
2025-06-11 21:35:08 +0800

    正常金幣會轉

--- Pickup/CoinPickup.cpp
+++ Pickup/CoinPickup.cpp
@@ -2,17 +2,36 @@
 #include "Scene/PlayScene.hpp"
 #include "Engine/Collider.hpp"
 #include "Engine/GameEngine.hpp"
+#include "Engine/Resources.hpp"
 
-PlayScene *CoinPickup::getPlayScene() {
-    return dynamic_cast<PlayScene *>(Engine::GameEngine::GetInstance().GetActiveScene());
+PlayScene* CoinPickup::getPlayScene() {
+    return dynamic_cast<PlayScene*>(Engine::GameEngine::GetInstance().GetActiveScene());
 }
 
 CoinPickup::CoinPickup(float x, float y, float value) :
-    Engine::Sprite("play/coin10-1.png", x, y , 0, 0, 0.25, 0.25), value(value) {}
+    Engine::Sprite("play/coin10-1.png", x, y, 0, 0, 0.25, 0.25),  // 用第一張圖初始化，並縮小為 25%
+    value(value),
+    frameCount(0),
+    frameInterval(10)
+{
+    coinImgs = {
+        "play/coin10-1.png",
+        "play/coin10-2.png",
+        "play/coin10-3.png"
+    };
+}
 
 void CoinPickup::Update(float deltaTime) {
     Engine::Sprite::Update(deltaTime);
     auto scene = getPlayScene();
+
+    frameCount++;
+    if (frameCount % frameInterval == 0) {
+        int index = (frameCount / frameInterval) % coinImgs.size();
+        bmp = Engine::Resources::GetInstance().GetBitmap(coinImgs[index]);
+        
+    }
+
     // 偵測玩家是否撿起
     // if (Engine::Collider::IsCircleOverlap(Position, 8, scene->Player->Position, 16)) {
     //     scene->EarnMoney(value); // 加錢
--- Pickup/CoinPickup.hpp
+++ Pickup/CoinPickup.hpp
@@ -1,13 +1,21 @@
 #ifndef COIN_PICKUP_HPP
 #define COIN_PICKUP_HPP
+#include <vector>
+#include <string>
 #include "Engine/Sprite.hpp"
 #include "Scene/PlayScene.hpp"
+
 class CoinPickup : public Engine::Sprite {
 public:
     float value;
     CoinPickup(float x, float y, float value);
     void Update(float deltaTime) override;
+
 protected:
-    PlayScene *getPlayScene();
+    PlayScene* getPlayScene();
+    int frameCount;
+    int frameInterval;
+    std::vector<std::string> coinImgs;
 };
+
 #endif
