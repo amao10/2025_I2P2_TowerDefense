f6162033d4ba171bd6b3a2d426bff88c0ffc19b7
tungchen <toni901231@gmail.com>
2025-06-11 21:35:45 +0800

    prone有bug/下跳還沒做，其他動畫都好了

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -3,78 +3,210 @@
 #include <allegro5/allegro_image.h>
 #include <allegro5/color.h>
 #include <allegro5/allegro.h>
+#include <algorithm>
 #include "Engine/Resources.hpp"
+#include "Scene/PlayScene.hpp"
+#include "Engine/GameEngine.hpp"
 
 Player::Player(int x, int y, int speed, int hp, int mp, int atk, int def)
     : Engine::Sprite("Player_no_weapon/stand1_0.png", x, y), speed(speed), hp(hp), maxHp(hp),
-      mp(mp), maxMp(mp), exp(0), attack(atk), defense(def), direction(LEFT) {
+      mp(mp), maxMp(mp), exp(0), attack(atk), defense(def), direction(LEFT), currentWeapon(UNARMED), proning(false), onRope(false) {
     LoadAnimation();
 }
 
-Player::~Player() {
-    
-}
+Player::~Player() {}
 
 void Player::LoadAnimation() {
-
-    standLeftFrames = {
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stand1_0.png").get(),
+    // Stand Animations
+    standAnimations[UNARMED] = {
         Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stand1_1.png").get(),
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stand1_2.png").get(),
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stand1_3.png").get()
+    };
+    standAnimations[SWORD] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/stand1_1.png").get(),
+    };
+    standAnimations[HANDCANNON] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/stand1_1.png").get(),
     };
 
-    walkUpRopeFrames = {
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/rope_0.png").get(),
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/rope_1.png").get(),
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/rope_2.png").get()
-    };
-
-    walkDownRopeFrames = {
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/rope_0.png").get(),
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/rope_1.png").get(),
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/rope_2.png").get()
-    };
-
-    walkLeftFrames = {
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/walk1_0.png").get(),
+    // Walk Animations
+    walkAnimations[UNARMED] = {
         Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/walk1_1.png").get(),
         Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/walk1_2.png").get(),
         Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/walk1_3.png").get(),
-        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/walk1_4.png").get()
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/walk1_2.png").get(),
+    };
+    walkAnimations[SWORD] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/walk1_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/walk1_2.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/walk1_3.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/walk1_2.png").get(),
+    };
+    walkAnimations[HANDCANNON] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/walk1_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/walk1_2.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/walk1_3.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/walk1_2.png").get(),
+    };
+    //Jump Animations
+    jumpAnimations[UNARMED] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/jump_0.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/jump_1.png").get(),
+    };
+
+    jumpAnimations[SWORD] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/jump_0.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/jump_1.png").get(),
+    };
+
+    jumpAnimations[HANDCANNON] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/jump_0.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/jump_1.png").get(),
+    };
+
+    proneAnimations[UNARMED] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/prone_0.png").get(),
+    };
+
+    proneAnimations[SWORD] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/prone_0.png").get(),
+    };
+
+    proneAnimations[HANDCANNON] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/prone_0.png").get(),
+    };
+    // Attack Animations
+    attackAnimations[UNARMED] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stabO2_0.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stabO2_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stabO2_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_no_weapon/stabO2_1.png").get()
+    };
+    attackAnimations[SWORD] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/stabO1_0.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/stabO1_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/stabO1_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/stabO1_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_sword/stabO1_1.png").get()
+        //Engine::Resources::GetInstance().GetBitmap("Player_sword/stabO1_2.png").get()
+    };
+    attackAnimations[HANDCANNON] = {
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/shootF_0.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/shootF_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/shootF_1.png").get(),
+        Engine::Resources::GetInstance().GetBitmap("Player_handcannon/shootF_1.png").get()
     };
 }
-
 
 void Player::Update(float deltaTime) {
     ALLEGRO_KEYBOARD_STATE keyState;
     al_get_keyboard_state(&keyState);
 
-    float moveDist = speed * deltaTime;
+    float moveDist = speed * deltaTime / 2;
     bool moved = false;
 
-    if (al_key_down(&keyState, ALLEGRO_KEY_UP)) {
-        Position.y -= moveDist;
-        direction = UP;
-        moved = true;
-    } else if (al_key_down(&keyState, ALLEGRO_KEY_DOWN)) {
-        Position.y += moveDist;
-        direction = DOWN;
-        moved = true;
-    } else if (al_key_down(&keyState, ALLEGRO_KEY_LEFT)) {
-        Position.x -= moveDist;
-        direction = LEFT;
-        moved = true;
-    } else if (al_key_down(&keyState, ALLEGRO_KEY_RIGHT)) {
-        Position.x += moveDist;
-        direction = RIGHT;
-        moved = true;
+    auto* scene = dynamic_cast<PlayScene*>(Engine::GameEngine::GetInstance().GetActiveScene());
+    int gridX = floor(Position.x / PlayScene::BlockSize);
+    int gridY = floor((Position.y + Size.y / 2 + 1) / PlayScene::BlockSize);
+
+    bool touchingFloor = false;
+    if (scene && gridX >= 0 && gridX < PlayScene::MapWidth && gridY >= 0 && gridY < PlayScene::MapHeight) {
+        if (!scene->mapState[gridY][gridX]) {
+            touchingFloor = true;
+        }
     }
 
-    if (moved) {
+    if (onGround && al_key_down(&keyState, ALLEGRO_KEY_ALT)) {
+        velocity.y = -400;
+        onGround = false;
+    }
+
+    //prone
+    proning = al_key_down(&keyState, ALLEGRO_KEY_DOWN) && !onRope;
+    
+    if (!onGround) {
+        velocity.y += 1000 * deltaTime;
+        Position.y += velocity.y * deltaTime;
+    }
+
+    if (touchingFloor && velocity.y >= 0) {
+        onGround = true;
+        velocity.y = 0;
+        Position.y = gridY * PlayScene::BlockSize - Size.y / 2;
+    } else {
+        onGround = false;
+    }
+
+    // 武器切換邏輯（按 SPACE）
+    static bool spacePressed = false;
+    if (al_key_down(&keyState, ALLEGRO_KEY_SPACE)) {
+        if (!spacePressed) {
+            // 切換武器：循環 UNARMED -> SWORD -> HANDCANNON -> UNARMED
+            currentWeapon = static_cast<WeaponType>((currentWeapon + 1) % 3);
+            animFrame = 0; // 切換時重置動畫
+            animTimer = 0;
+            spacePressed = true;
+        }
+    } else {
+        spacePressed = false; // 放開鍵後才能再次觸發
+    }
+
+
+    int screenW = Engine::GameEngine::GetInstance().GetScreenSize().x;
+    int screenH = Engine::GameEngine::GetInstance().GetScreenSize().y;
+    Position.x = std::max(Size.x / 2.0f, std::min(Position.x, screenW - Size.x / 2.0f));
+    Position.y = std::min(Position.y, screenH - Size.y / 2.0f);
+
+    if (!al_key_down(&keyState, ALLEGRO_KEY_LCTRL)) {
+        attackKeyPressed = false;
+    }
+    if (al_key_down(&keyState, ALLEGRO_KEY_LCTRL) && !attackKeyPressed && !attacking) {
+        attacking = true;
+        attackKeyPressed = true;
+        attackDashed = false;
+        attackTimer = 0;
+        animFrame = 0;
+    }
+
+    if (!attacking) {
+        if (al_key_down(&keyState, ALLEGRO_KEY_UP)) {
+            Position.y -= moveDist;
+            direction = UP;
+            moved = true;
+        } else if (al_key_down(&keyState, ALLEGRO_KEY_DOWN)) {
+            Position.y += moveDist;
+            direction = DOWN;
+            moved = true;
+        } else if (al_key_down(&keyState, ALLEGRO_KEY_LEFT)) {
+            Position.x -= moveDist;
+            direction = LEFT;
+            moved = true;
+        } else if (al_key_down(&keyState, ALLEGRO_KEY_RIGHT)) {
+            Position.x += moveDist;
+            direction = RIGHT;
+            moved = true;
+        }
+    }
+
+    if (attacking) {
+        attackTimer += deltaTime;
+        int totalFrames = attackAnimations[currentWeapon].size();
+        int currentFrame = int(attackTimer / attackDuration * totalFrames);
+        animFrame = std::min(currentFrame, totalFrames - 1);
+
+        if ((animFrame == 0 || animFrame == 1) && !attackDashed) {
+            float dashAmount = 300 * deltaTime;
+            Position.x += (direction == RIGHT ? dashAmount : -dashAmount);
+            attackDashed = true;
+        }
+
+        if (attackTimer >= attackDuration) {
+            attacking = false;
+            animFrame = 0;
+        }
+    } else if (moved) {
         animTimer += deltaTime;
         if (animTimer >= 0.3f) {
-            animFrame = (animFrame + 1) % 3;
+            animFrame = (animFrame + 1) % 4;
             animTimer = 0;
         }
     } else {
@@ -90,52 +222,46 @@ void Player::Draw() const {
     const std::vector<ALLEGRO_BITMAP*>* currentFrames = nullptr;
     bool isFlipped = false;
 
-    // ── 停止移動時 ──
-    if (animFrame == 0 && animTimer == 0) {
-        switch (direction) {
-            case UP:
-                bmp = walkUpRopeFrames.empty() ? nullptr : walkUpRopeFrames[0];
-                break;
-            case DOWN:
-                bmp = walkDownRopeFrames.empty() ? nullptr : walkDownRopeFrames[0];
-                break;
-            case LEFT:
-                bmp = walkLeftFrames.empty() ? nullptr : walkLeftFrames[1];
-                break;
-            case RIGHT:
-                bmp = walkLeftFrames.empty() ? nullptr : walkLeftFrames[1];
-                isFlipped = true;
-                break;
-        }
+    if (attacking && !attackAnimations[currentWeapon].empty()) {
+        bmp = attackAnimations[currentWeapon][animFrame % attackAnimations[currentWeapon].size()];
+        al_draw_bitmap(bmp,
+            Position.x - al_get_bitmap_width(bmp) / 2,
+            Position.y - al_get_bitmap_height(bmp) / 2,
+            direction == RIGHT ? ALLEGRO_FLIP_HORIZONTAL : 0);
+        return;
     }
-    // ── 有移動動畫時 ──
-    else {
-        switch (direction) {
-            case UP:
-                currentFrames = &walkUpRopeFrames;
-                break;
-            case DOWN:
-                currentFrames = &walkDownRopeFrames;
-                break;
-            case LEFT:
-                currentFrames = &walkLeftFrames;
-                break;
-            case RIGHT:
-                currentFrames = &walkLeftFrames;
-                isFlipped = true;
-                break;
-        }
 
+    if (!onGround && !jumpAnimations[currentWeapon].empty()) {
+        bmp = jumpAnimations[currentWeapon][animFrame % jumpAnimations[currentWeapon].size()];
+        al_draw_bitmap(bmp,
+            Position.x - al_get_bitmap_width(bmp) / 2,
+            Position.y - al_get_bitmap_height(bmp) / 2,
+            direction == RIGHT ? ALLEGRO_FLIP_HORIZONTAL : 0);
+        return;
+    }
+
+    if (proning && !proneAnimations[currentWeapon].empty()) {
+        bmp = proneAnimations[currentWeapon][0];
+        al_draw_bitmap(bmp,
+            Position.x - al_get_bitmap_width(bmp) / 2,
+            Position.y - 5,
+            direction == RIGHT ? ALLEGRO_FLIP_HORIZONTAL : 0);
+        return;
+    }
+    //Position.y - al_get_bitmap_height(bmp) / 2,
+    if (animFrame == 0 && animTimer == 0) {
+        bmp = standAnimations[currentWeapon].empty() ? nullptr : standAnimations[currentWeapon][0];
+    } else {
+        currentFrames = &walkAnimations[currentWeapon];
         if (currentFrames && !currentFrames->empty()) {
             bmp = (*currentFrames)[animFrame % currentFrames->size()];
         }
     }
 
-    // ── 畫出圖片 ──
     if (bmp) {
         al_draw_bitmap(bmp,
             Position.x - al_get_bitmap_width(bmp) / 2,
             Position.y - al_get_bitmap_height(bmp) / 2,
-            isFlipped ? ALLEGRO_FLIP_HORIZONTAL : 0);
+            direction == RIGHT ? ALLEGRO_FLIP_HORIZONTAL : 0);
     }
 }
--- Player/Player.hpp
+++ Player/Player.hpp
@@ -2,9 +2,14 @@
 #define PLAYER_HPP
 
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/color.h>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "Engine/Sprite.hpp"
+#include "Engine/Point.hpp"
 
 class Player : public Engine::Sprite {
 protected:
@@ -13,19 +18,31 @@ protected:
     int mp, maxMp;
     int exp;
     int attack, defense;
-    //bool onRope;
 
-    std::vector<ALLEGRO_BITMAP*> standLeftFrames; //standRight:flip stand left
+    enum Direction { UP, DOWN, LEFT, RIGHT } direction;
+    enum WeaponType { UNARMED, SWORD, HANDCANNON };
+    WeaponType currentWeapon = UNARMED;
 
-
-    std::vector<ALLEGRO_BITMAP*> walkUpRopeFrames;
-    std::vector<ALLEGRO_BITMAP*> walkDownRopeFrames;
-    std::vector<ALLEGRO_BITMAP*> walkLeftFrames;
-    std::vector<ALLEGRO_BITMAP*> walkRightFrames;
+    std::vector<ALLEGRO_BITMAP*> standAnimations[3];
+    std::vector<ALLEGRO_BITMAP*> walkAnimations[3];
+    std::vector<ALLEGRO_BITMAP*> jumpAnimations[3];
+    std::vector<ALLEGRO_BITMAP*> proneAnimations[3];
+    std::vector<ALLEGRO_BITMAP*> attackAnimations[3];
 
     float animTimer = 0;
     int animFrame = 0;
-    enum Direction { UP, DOWN, LEFT, RIGHT } direction;
+
+    bool attacking = false;
+    float attackTimer = 0;
+    float attackDuration = 0.4f;
+    bool attackKeyPressed = false;
+    bool attackDashed = false;
+
+    Engine::Point velocity;
+    bool onGround = false;
+
+    bool proning = false;
+    bool onRope = false;
 
     void LoadAnimation();
 
@@ -34,6 +51,7 @@ public:
     ~Player();
     void Update(float deltaTime) override;
     void Draw() const override;
+    void SetWeapon(WeaponType weapon); // switch weapon
 };
 
 #endif  // PLAYER_HPP
